f9ad06bae8525f3bff88ec3a13da4a3d8b6c84e1
nick <r951236958@gmail.com>
2021-03-02 15:15:33 +0800

    使用useEffect

--- src/app/components/PrivateRoute.jsx
+++ src/app/components/PrivateRoute.jsx
@@ -1,14 +1,18 @@
 import { navigate } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 import { isLoggedIn } from "../../utils/auth"
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
+
+  useEffect(() => {
   if (!isLoggedIn() && location.pathname !== `/app/login`) {
     // If we’re not logged in, redirect to the home page.
     navigate(`/app/login`, { replace: true })
     return null
   }
+ 
+  }, [location])
 
   return <Component {...rest} />
 }
